ac132d166f6ec38fcc5bc54428d7fc7c1d114edb
ashishkum25 <130741156+ashishkum25@users.noreply.github.com>
2024-09-28 17:36:16 +0530

    Update index.js

--- init/index.js
+++ init/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-mongoose.connect(`mongodb+srv://webashish44:Wildcraft17@cluster0.nqcyyfe.mongodb.net/TripNStay`);
+mongoose.connect(``);
 const initDB = async () => {
   await Listing.deleteMany({});
   await Listing.insertMany(initData.data);
